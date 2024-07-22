86cb8abe512c8f6dce3067ab712e3f66f940a2cd
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2024-07-22 21:38:27 +0530

    Time: 159 ms (47.7%), Space: 120.1 MB (37.94%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -10,24 +10,126 @@
  * };
  */
 class Solution {
-public:
-    bool find(TreeNode* n, int val, string& path) {
-        if (n->val == val)
-            return true;
-        if (n->left && find(n->left, val, path))
-            path.push_back('L');
-        else if (n->right && find(n->right, val, path))
-            path.push_back('R');
-        return !path.empty();
-    }
-    string getDirections(TreeNode* root, int startValue, int destValue) {
-        string s_p, d_p;
-        find(root, startValue, s_p);
-        find(root, destValue, d_p);
-        while (!s_p.empty() && !d_p.empty() && s_p.back() == d_p.back()) {
-            s_p.pop_back();
-            d_p.pop_back();
+    int key = -1;
+    TreeNode* lca = new TreeNode();
+
+    bool help(TreeNode * root , string& s)
+    {
+
+        if (root == nullptr)
+        {
+            return false;
         }
-        return string(s_p.size(), 'U') + string(rbegin(d_p), rend(d_p));
+
+        if (root->val == key)
+        {
+            return true;
+        }
+
+        
+        bool a = help(root->right,s);
+        bool b = help(root->left,s);
+        s += "L";
+        if (!b)
+        {
+           s.pop_back();
+        }
+
+        s += "R";
+        if ( !a)
+        {
+            s.pop_back();
+        }
+
+        return a||b;
+         
+    }
+    bool find(TreeNode * root , TreeNode * sec  ,int b)
+    {  
+        if (root == nullptr) return false;
+        if (root == sec)
+        {
+            return false;
+        }
+
+        if (root->val == b) return true;
+
+        return find(root->left , sec,b)||find(root->right,sec,b);
+    }
+    bool func(TreeNode * root , vector<TreeNode*>&temp)
+    {  
+    if (root == NULL)
+       {
+        return false;
+       }
+
+    
+     if (root->val == key)
+       {
+        temp.push_back(root);
+        return true;
+       }
+
+     temp.push_back(root);
+     if (!func(root->left,temp) && !func(root->right,temp))
+       {
+          temp.pop_back();
+          return false;
+       }
+
+
+       return true;
+    }
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+
+        if (startValue == destValue)return "";
+        vector<TreeNode*>v1,v2;
+
+        key = startValue;
+        func(root,v1);
+
+        
+        int i = v1.size()-1;
+        int j = v2.size()-1;
+
+        TreeNode* lca = new TreeNode();
+
+        unordered_set<TreeNode*>st(v2.begin(),v2.end());
+         TreeNode* sec = new TreeNode();
+        while(i>=0)
+        {    
+            if(find(v1[i],sec,destValue))
+            {
+                lca = v1[i];
+                break;
+            }
+            sec = v1[i];
+            i--;
+            
+        }
+         
+         cout<<lca->val;
+       
+
+        string part1="",part2="";
+        key = startValue;
+        help(lca,part1);
+        
+
+        key = destValue;
+        help(lca,part2);
+
+        for(auto &it : part1)
+        {   
+            it  = 'U';
+        }
+
+        reverse(part2.begin(),part2.end());
+
+        string ans = part1 + part2;
+
+        return ans;
+
     }
 };
