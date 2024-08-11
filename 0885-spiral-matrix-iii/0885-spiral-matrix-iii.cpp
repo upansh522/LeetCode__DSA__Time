03a9aaf5435d6e49240ed2ac8194719d1eb0f2b6
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2024-08-11 16:20:57 +0530

    Time: 14 ms (23.76%), Space: 13.9 MB (42.88%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart,
-                                        int cStart) {
-        // Store all possible directions in an array.
-        vector<vector<int>> dir{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        vector<vector<int>> traversed;
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> ans;
+        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // Right, Down, Left, Up
+        int d = 0, steps = 1;  // d is the direction index, steps is the number of steps in the current direction
 
-        // Initial step size is 1, value of d represents the current direction.
-        for (int step = 1, direction = 0; traversed.size() < rows * cols;) {
-            // direction = 0 -> East, direction = 1 -> South
-            // direction = 2 -> West, direction = 3 -> North
-            for (int i = 0; i < 2; ++i) {
-                for (int j = 0; j < step; ++j) {
-                    // Validate the current position
-                    if (rStart >= 0 && rStart < rows && cStart >= 0 &&
-                        cStart < cols) {
-                        traversed.push_back({rStart, cStart});
+        int currRow = rStart, currCol = cStart;
+
+        ans.push_back({currRow, currCol});
+        while (ans.size() < rows * cols) {
+            for (int i = 0; i < 2; i++) {  // There are 2 turns for each complete cycle of directions
+                for (int j = 0; j < steps; j++) {
+                    currRow += directions[d][0];
+                    currCol += directions[d][1];
+                    if (currRow >= 0 && currRow < rows && currCol >= 0 && currCol < cols) {
+                        ans.push_back({currRow, currCol});
                     }
-                    // Make changes to the current position.
-                    rStart += dir[direction][0];
-                    cStart += dir[direction][1];
                 }
-
-                direction = (direction + 1) % 4;
+                d = (d + 1) % 4;  // Change direction
             }
-            ++step;
+            steps++;  // Increase the number of steps after every two directions (right-down or left-up)
         }
-        return traversed;
+
+        return ans;
     }
 };
