01357f3219b19050dc3e29346afc1dd73b05714d
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2024-08-22 23:47:06 +0530

    Time: 141 ms (5.47%), Space: 43.3 MB (76.25%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+int find(vector<int> &parent, int x)
+{
+    if (parent[x]==x)
+    return x;
+    return find(parent, parent[x]);
+}
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        vector<int> parent(n),size(n,1);
+        for (int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+
+        int extra=0;
+
+        for (int i=0;i<connections.size();i++)
+        {
+            int u=connections[i][0];
+            int v=connections[i][1];
+            int up_u=find(parent,u);
+            int up_v=find(parent,v);
+            if (up_u==up_v)
+            {
+                extra ++;
+                continue;
+            }
+
+            if (size[up_u]>size[up_v])
+            {
+                parent[up_v]=parent[up_u];
+                size[up_u]+=size[up_v];
+            }
+            else {
+                parent[up_u]=parent[up_v];
+                size[up_v]=size[up_u];
+            }
+
+        }
+
+        unordered_set<int> st;
+        for (int i=0;i<n;i++)
+        {
+            int up=find(parent,i);
+            st.insert(up);
+        }
+
+        if (st.size()>extra+1)
+        return -1;
+        
+        return st.size()-1;        
+    }
+};
