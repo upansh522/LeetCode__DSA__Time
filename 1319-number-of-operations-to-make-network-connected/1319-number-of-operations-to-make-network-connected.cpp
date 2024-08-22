5ed602fe6385402f0b69fd64515c5d97f2e4ea7c
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2024-08-22 23:51:03 +0530

    Time: 159 ms (5.12%), Space: 43.2 MB (76.25%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
-    int find(vector<int> &parent, int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent, parent[x]);  // Path compression
-    }
-    
+int find(vector<int> &parent, int x)
+{
+    if (parent[x]==x)
+    return x;
+    return find(parent, parent[x]);
+}
     int makeConnected(int n, vector<vector<int>>& connections) {
         if (connections.size() < n - 1) {
             // If there are fewer than n-1 edges, it's impossible to connect all computers
             return -1;
         }
-
-        vector<int> parent(n), size(n, 1);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        vector<int> parent(n),size(n,1);
+        for (int i=0;i<n;i++)
+        {
+            parent[i]=i;
         }
 
-        int extra = 0;
+        int extra=0;
 
-        for (int i = 0; i < connections.size(); i++) {
-            int u = connections[i][0];
-            int v = connections[i][1];
-            int up_u = find(parent, u);
-            int up_v = find(parent, v);
-            if (up_u == up_v) {
-                extra++;
+        for (int i=0;i<connections.size();i++)
+        {
+            int u=connections[i][0];
+            int v=connections[i][1];
+            int up_u=find(parent,u);
+            int up_v=find(parent,v);
+            if (up_u==up_v)
+            {
+                extra ++;
                 continue;
             }
 
-            if (size[up_u] > size[up_v]) {
-                parent[up_v] = up_u;
-                size[up_u] += size[up_v];  // Correctly updating size
-            } else {
-                parent[up_u] = up_v;
-                size[up_v] += size[up_u];  // Correctly updating size
+            if (size[up_u]>size[up_v])
+            {
+                parent[up_v]=parent[up_u];
+                size[up_u]+=size[up_v];
             }
+            else {
+                parent[up_u]=parent[up_v];
+                size[up_v]=size[up_u];
+            }
+
         }
 
-        unordered_set<int> components;
-        for (int i = 0; i < n; i++) {
-            components.insert(find(parent, i));
+        unordered_set<int> st;
+        for (int i=0;i<n;i++)
+        {
+            int up=find(parent,i);
+            st.insert(up);
         }
 
-        int components_count = components.size();
+        if (st.size()>extra+1)
+        return -1;
         
-        // If extra connections are enough to connect all components
-        if (extra >= components_count - 1) {
-            return components_count - 1;
-        } else {
-            return -1;
-        }
+        return st.size()-1;        
     }
 };
