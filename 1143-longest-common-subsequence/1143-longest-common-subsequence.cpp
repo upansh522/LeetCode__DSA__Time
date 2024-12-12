67ab5cc9f676b0d74d015081d2bd1c4815f27db2
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2024-12-12 22:27:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int f(string &txt1, string &txt2, int ind, int fromInd, vector<vector<int>> &dp) {
-        if (ind == txt1.size()) {
-            return 0; // Base case: Reached the end of txt1.
-        }
+int f(string txt1, string txt2, int ind, int fromInd, vector<vector<int>>&dp){
+    char c=txt1[ind];
+    if (ind==txt1.size()-1){
+        return (txt2.find(c,fromInd)!=string::npos)?1:0;
+    }
+    if (dp[ind][fromInd]!=-1)
+    return dp[ind][fromInd];
 
-        if (dp[ind][fromInd] != -1) {
-            return dp[ind][fromInd]; // Return memoized result if available.
-        }
-
-        char c = txt1[ind]; // Current character from txt1.
-        int notTaken = f(txt1, txt2, ind + 1, fromInd, dp); // Skip this character.
-        int taken = 0;
-
-        size_t pos = txt2.find(c, fromInd); // Find the character in txt2 starting from `fromInd`.
-        if (pos != string::npos) {
-            // If the character is found, continue the recursion.
-            taken = 1 + f(txt1, txt2, ind + 1, pos + 1, dp);
-        }
-
-        // Store the result in the dp table and return the maximum.
-        return dp[ind][fromInd] = max(taken, notTaken);
+    int notTaken=f(txt1,txt2,ind+1,fromInd,dp);
+    int taken=0;
+    int n=txt2.find(c,fromInd);
+    if (n!=string::npos){
+        fromInd=n;
+        taken=f(txt1, txt2, ind+1, n, dp)+1;
     }
 
+    return dp[ind][fromInd]=max(taken,notTaken);
+}
     int longestCommonSubsequence(string text1, string text2) {
-        int n1 = text1.size();
-        int n2 = text2.size();
-
-        // Use a 2D dp array to store results for all combinations of `ind` and `fromInd`.
-        vector<vector<int>> dp(n1, vector<int>(n2 + 1, -1));
-
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
         return f(text1, text2, 0, 0, dp);
     }
 };
