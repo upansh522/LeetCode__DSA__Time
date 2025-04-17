78fed4e4f8dae7359fd7f067983e81f9fc77a594
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2025-04-17 15:48:48 +0530

    Time: 239 ms (5.01%), Space: 198.9 MB (59.68%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -11,8 +11,8 @@ int intToBinary(int num){
 }
     int uniqueXorTriplets(vector<int>& nums) {
         if (nums.size()<3)return nums.size();
-        sort(nums.begin(),nums.end());
-        int block=intToBinary(nums.back());
+        int num=*max_element(nums.begin(),nums.end());
+        int block=intToBinary(num);
         return pow(2,block) ;
     }
 };
