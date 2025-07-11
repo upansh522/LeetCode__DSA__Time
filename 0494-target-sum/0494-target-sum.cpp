cd97253da452ffe4c10b1e5fb41b6a49f14ba276
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2025-07-11 14:07:56 +0530

    Time: 127 ms (39.1%), Space: 53.1 MB (16.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int target, int sum, vector<vector<int>>& dp, int ind) {
-        int n = nums.size();
-        if (ind == n) {
-            return (target == 0) ? 1 : 0; // Base case: one valid way if target is achieved
-        }
-        if (target + sum < 0 || target + sum >= 2 * sum + 1) {
-            return 0;
-        }
+int f(vector<int>& nums, int target, int ind, vector<unordered_map<int, int>>&dp){
+    if(ind==0){
+        int a=target-nums[ind];
+        int s=target+nums[ind];
+        if (a==0 && s==0)return 2;
+        else if (a==0||s==0)return 1;
+        else return 0;
+    } 
 
-        if (dp[target + sum][ind] != -1) {
-            return dp[target + sum][ind];
-        }
-
-        // Recursive calls
-        int add = f(nums, target - nums[ind], sum, dp, ind + 1);
-        int sub = f(nums, target + nums[ind], sum, dp, ind + 1);
-
-        return dp[target + sum][ind] = add + sub;
-    }
-
+    if (dp[ind].find(target)!=dp[ind].end())return dp[ind][target];
+    int add=f(nums, target-nums[ind], ind-1, dp);
+    int sub=f(nums, target+nums[ind], ind-1, dp);
+    return dp[ind][target]=add+sub;
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (abs(target) > sum) {
-            return 0; // If target is not achievable, return 0
-        }
-
-        int n = nums.size();
-        vector<vector<int>> dp(2 * sum + 1, vector<int>(n, -1));
-
-        return f(nums, target, sum, dp, 0);
+        int n=nums.size();
+        vector<unordered_map<int, int>>dp (n);
+        return f(nums,target, n-1, dp);
     }
 };
