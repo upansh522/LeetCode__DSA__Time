00d3ae4e8203054f7e48849c52fbc90f33aedd33
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2025-07-29 21:16:28 +0530

    Time: 26 ms (33.93%), Space: 38.8 MB (44.35%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
+    vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat[0].size();
-        int m=mat.size();
-        vector<vector<int>> visited(m,vector<int>(n,0));
-        queue<pair<int,pair<int,int>>>qu;
-        vector<vector<int>> ans(m, vector<int>(n, 0));
-        for (int i=0;i<m;i++)
-        {
-            for (int j=0;j<n;j++)
-            {
-                if (mat[i][j]==0)
-                {visited[i][j]=1;
-                qu.push({i,{j,0}});}
+        int m = mat.size();
+        int n = mat[0].size();
+        queue<pair<int,pair<int,int>>>q;
+        vector<vector<int>>visited(m,vector<int>(n,0)),ans(m,vector<int>(n,0));
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (mat[i][j]==0){
+                    q.push({0,{i,j}});
+                visited[i][j]=1;
+                }
             }
         }
 
-        while(!qu.empty())
-        {
-            int row=qu.front().first;
-            int col=qu.front().second.first;
-            int path=qu.front().second.second;
-            qu.pop();
-            ans[row][col]=path;
-
-            if (row+1<m && visited[row+1][col]==0)
-            {
-                visited[row+1][col]=1;
-                qu.push({row+1,{col,path+1}});
-            }
-            if (row-1>=0 && visited[row-1][col]==0)
-            {
-                visited[row-1][col]=1;
-                qu.push({row-1,{col,path+1}});
-            }
-            if (col-1>=0 && visited[row][col-1]==0)
-            {
-                visited[row][col-1]=1;
-                qu.push({row,{col-1,path+1}});
-            }
-            if (col+1<n && visited[row][col+1]==0)
-            {
-                visited[row][col+1]=1;
-                qu.push({row,{col+1,path+1}});
+        while(!q.empty()){
+            auto [step,pos]=q.front();
+            auto [row,col]=pos;
+            q.pop();
+            ans[row][col]=step;
+            for (auto it:dir){
+                int newRow=row+it.first;
+                int newCol=col+it.second;
+                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
+                    if (visited[newRow][newCol]==0){
+                        q.push({step+1,{newRow,newCol}});
+                        visited[newRow][newCol]=1;
+                    }
+                }
             }
         }
 
-        return ans;
+        return ans        ;
     }
 };
