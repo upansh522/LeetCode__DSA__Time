3b90cb0f555ce31213a559bbe6c3d79140ac5da4
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2025-09-29 00:53:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
+int f(vector<int>&nums, vector<int>&dp, int currHouse){
+    if (currHouse<0)return 0;
+    if (currHouse==0)return dp[0];
 
-int take_first(vector<int> &nums, vector<int>&dp, int currHouse)
-{
-    if (currHouse==0)
-    {
-        return dp[0];
-    }
-    if (currHouse<0){
-        return 0;
-    }
+    if (dp[currHouse]!=-1)return dp[currHouse];
 
-    if (dp[currHouse]!=-1){
-        return dp[currHouse];
-    }
-    else {
-        return dp[currHouse]=max(nums[currHouse]+take_first(nums,dp,currHouse-2),take_first(nums,dp,currHouse-1));
-    }
+    return dp[currHouse]=max(nums[currHouse]+f(nums,dp,currHouse-2), f(nums, dp,currHouse-1));
 }
-
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if (n==1)
-        return nums[0];
+        vector<int>dp(n,-1),dpp(n,-1);
         
-        vector<int> dp(n,-1);
-        vector<int> dpp(n,-1);
         dp[0]=0;
-        int take_last=take_first(nums,dp,n-1);
+        int take=f(nums,dp,n-1);
         dpp[0]=nums[0];
-        int incl_first=take_first(nums,dpp,n-2);
+        int notTake=f(nums,dpp,n-2);
 
-        return max(incl_first,take_last);
+        return max(take,notTake);
     }
 };
