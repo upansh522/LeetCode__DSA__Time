311fd725fd4d96385d39466d824f7904a562910f
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2025-09-29 12:04:44 +0530

    Time: 6 ms (3.99%), Space: 16.7 MB (6.65%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
+long long f(vector<vector<int>>& grid, int row, int col, vector<unordered_map<int,int>>&dp){
+    int n=grid.size();
+    if (row==n-1){
+        return grid[row][col];
+    }
+
+    if (dp[row].find(col)!=dp[row].end())return dp[row][col];
+
+    return dp[row][col]=grid[row][col]+min(f(grid, row+1, col, dp), f(grid, row+1, col+1, dp));
+}
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for (int i=0;i<n;i++)
-        {
-            for (int j=0;j<triangle[i].size();j++){
-                if (i==0 && j==0){
-                    dp[0][0]=triangle[i][j];
-                }else{
-                    int sameInd=triangle[i][j];
-                    if(j<triangle[i].size()-1){
-                        sameInd+=dp[i-1][j];
-                    }
-                    else sameInd=INT_MAX;
-
-                    int sameCol=triangle[i][j];
-                    if (j>0)
-                    sameCol+=dp[i-1][j-1];
-                    else{
-                        sameCol=INT_MAX;
-                    }
-                   dp[i][j]=min(sameCol,sameInd);
-                }
-            }
-        }
-
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
+        vector<unordered_map<int,int>>dp(n);
+        return f(triangle, 0, 0, dp);
     }
 };
