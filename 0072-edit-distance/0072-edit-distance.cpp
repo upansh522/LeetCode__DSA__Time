6d6421c56cff661f443b8a3c9b2658740205cacd
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2026-07-01 00:28:57 +0530

    Time: 63 ms (9.61%), Space: 84.3 MB (5.12%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int f(string &s, string &t, int i, int j, vector<vector<int>> &dp){
-        if (i == s.size()) return t.size() - j;
-        if (j == t.size()) return s.size() - i;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s[i] == t[j]) {
-            return dp[i][j] = f(s, t, i+1, j+1, dp);
-        }
-
-        int insert = 1 + f(s, t, i, j+1, dp);   // Insert character in s
-        int del = 1 + f(s, t, i+1, j, dp);      // Delete character from s
-        int rep = 1 + f(s, t, i+1, j+1, dp);    // Replace character in s
-
-        return dp[i][j] = min({insert, del, rep});
+int f(string s, string t, int i, int j, vector<vector<int>>&dp){
+    if (i<0)return j+1;
+    if (j<0)return i+1;
+    if (dp[i][j]!=INT_MAX){
+        return dp[i][j];
     }
 
+    if (s[i]==t[j]){
+        return dp[i][j]=f(s, t, i-1, j-1, dp);
+    }
+    int delOps=f(s, t, i-1, j, dp);
+    int insOps=f(s,t,i,j-1, dp);
+    int repOps=f(s, t, i-1, j-1, dp);
+    return dp[i][j]=1+min({delOps, insOps, repOps});
+}
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(word1, word2, 0, 0, dp);
+        int m=word1.size(), n=word2.size();
+        vector<vector<int>>dp(m, vector<int>(n,INT_MAX));
+        return f(word1, word2, m-1, n-1, dp);        
     }
 };
