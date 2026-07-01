608d9981982f20c174507b7aeb76c7e8d1fc03fe
Nishant Upadhyay <127209982+upansh522@users.noreply.github.com>
2026-07-01 22:46:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-int f(int m, int n, vector<vector<int>>&dp, int currRow,int currCol){
-    if (currRow==m || currCol==n){
+vector<pair<int,int>>dir={{1,0}, {0,1}};
+int f(int m, int n, int currRow, int currCol, vector<vector<int>>& dp){
+    if (currRow>=m || currCol>=n){
         return 0;
     }
-    if (currRow==m-1 && currCol==n-1){
-        return 1;
-    }
-    vector<pair<int,int>> dir={{1,0},{0,1}};
-
-    for (auto [increRow,increCol]: dir){
-        if (dp[currRow+increRow][currCol+increCol]!=0){
-            dp[currRow][currCol]+=dp[currRow+increRow][currCol+increCol];
+    if (currRow==m-1 && currCol==n-1)return 1;
+    for (auto it:dir){
+        int row=it.first;
+        int col=it.second;
+        if (dp[currRow+row][currCol+col]!=0){
+            dp[currRow][currCol]+=dp[currRow+row][currCol+col];
         }
-        else {
-            dp[currRow][currCol]+=f(m,n,dp,currRow+increRow,currCol+increCol);
+        else{
+            dp[currRow][currCol]+=f(m, n, currRow+row, currCol+col, dp);
         }
     }
-
     return dp[currRow][currCol];
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        return f(m,n,dp,0,0);
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        return f(m, n, 0, 0,  dp);
     }
 };
